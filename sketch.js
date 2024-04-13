b079b7c03d30b50daa508005e9ba381cf7d66e82
red-straw <144910907+red-straw@users.noreply.github.com>
2024-04-13 21:57:20 +0900

    불필요한 줄바꿈 삭제 function draw()에서 음식 색상 및 모양 변경 this.show = function()에서 뱀의 머리 및 꼬리 모양 변경

--- sketch.js
+++ sketch.js
@@ -1,129 +1,71 @@
-//Delcare Global Variables
-
 let s;
-
 let scl = 20;
-
 let food;
-
 playfield = 600;
 
- 
-
 // p5js Setup function - required
-
- 
 
 function setup() {
 
   createCanvas(playfield, 640);
-
   background(51);
-
   s = new Snake();
-
   frameRate (10);
-
   pickLocation();
-
 }
 
- 
-
 // p5js Draw function - required
-
- 
 
 function draw() {
 
   background(51);
-
   scoreboard();
-
   if (s.eat(food)) {
-
     pickLocation();
-
   }
 
   s.death();
-
   s.update();
-
   s.show();
 
- 
-
-  fill (255,0,100);
-
-  rect(food.x,food.y, scl, scl);
+  fill("yellow"); // 음식의 색상 설정
+  ellipse(food.x + scl / 2, food.y + scl / 2, scl, scl); // 음식 모양 변경
 
 }
 
- 
-
 // Pick a location for food to appear
-
- 
 
 function pickLocation() {
 
   let cols = floor(playfield/scl);
-
   let rows = floor(playfield/scl);
-
   food = createVector(floor(random(cols)), floor(random(rows)));
-
   food.mult(scl);
-
- 
 
   // Check the food isn't appearing inside the tail
 
- 
-
   for (let i = 0; i < s.tail.length; i++) {
-
     let pos = s.tail[i];
-
     let d = dist(food.x, food.y, pos.x, pos.y);
-
     if (d < 1) {
-
       pickLocation();
-
     }
-
   }
-
 }
 
- 
-
 // scoreboard
-
- 
 
 function scoreboard() {
 
   fill(0);
-
   rect(0, 600, 600, 40);
-
   fill(255);
-
   textFont("Georgia");
-
   textSize(18);
-
   text("Score: ", 10, 625);
-
   text("Highscore: ", 450, 625)
-
   text(s.score, 70, 625);
-
   text(s.highscore, 540, 625)
-
 }
 
  
@@ -135,23 +77,14 @@ function scoreboard() {
 function keyPressed() {
 
   if (keyCode === UP_ARROW){
-
       s.dir(0, -1);
-
   }else if (keyCode === DOWN_ARROW) {
-
       s.dir(0, 1);
-
   }else if (keyCode === RIGHT_ARROW) {
-
       s.dir (1, 0);
-
   }else if (keyCode === LEFT_ARROW) {
-
       s.dir (-1, 0);
-
   }
-
 }
 
  
@@ -163,19 +96,12 @@ function keyPressed() {
 function Snake() {
 
   this.x =0;
-
   this.y =0;
-
   this.xspeed = 1;
-
   this.yspeed = 0;
-
   this.total = 0;
-
   this.tail = [];
-
   this.score = 1;
-
   this.highscore = 1;
 
  
@@ -183,9 +109,7 @@ function Snake() {
   this.dir = function(x,y) {
 
     this.xspeed = x;
-
     this.yspeed = y;
-
   }
 
  
@@ -193,31 +117,19 @@ function Snake() {
   this.eat = function(pos) {
 
     let d = dist(this.x, this.y, pos.x, pos.y);
-
     if (d < 1) {
-
       this.total++;
-
       this.score++;
-
       text(this.score, 70, 625);
-
       if (this.score > this.highscore) {
-
         this.highscore = this.score;
-
       }
-
       text(this.highscore, 540, 625);
-
       return true;
-
-    } else {
-
+    } 
+    else {
       return false;
-
     }
-
   }
 
  
@@ -225,77 +137,53 @@ function Snake() {
   this.death = function() {
 
     for (let i = 0; i < this.tail.length; i++) {
-
       let pos = this.tail[i];
-
       let d = dist(this.x, this.y, pos.x, pos.y);
-
       if (d < 1) {
-
         this.total = 0;
-
         this.score = 0;
-
         this.tail = [];
-
       }
-
     }
-
   }
 
  
 
   this.update = function(){
-
     if (this.total === this.tail.length) {
-
       for (let i = 0; i < this.tail.length-1; i++) {
-
         this.tail[i] = this.tail[i+1];
-
     }
-
- 
-
     }
 
     this.tail[this.total-1] = createVector(this.x, this.y);
-
- 
-
     this.x = this.x + this.xspeed*scl;
-
     this.y = this.y + this.yspeed*scl;
 
- 
-
     this.x = constrain(this.x, 0, playfield-scl);
-
     this.y = constrain(this.y, 0, playfield-scl);
-
- 
-
- 
 
   }
 
   this.show = function(){
-
-    fill(255);
-
-    for (let i = 0; i < this.tail.length; i++) {
-
-        rect(this.tail[i].x, this.tail[i].y, scl, scl);
-
-    }
-
- 
-
-    rect(this.x, this.y, scl, scl);
-
+  fill(255);
+  for (let i = 0; i < this.tail.length; i++) {
+      fill("yellow");
+      ellipse(this.tail[i].x + scl / 2, this.tail[i].y + scl / 2, scl, scl);
   }
-
- 
+  fill("yellow"); // 뱀의 머리를 노란색으로 채우기
+  // 방향키에 따라 뱀의 머리 방향 전환
+  if (this.yspeed === -1) { // 위로 이동 중
+    arc(this.x + scl / 2, this.y + scl / 2, scl, scl, radians(315), radians(225));
+  } else if (this.yspeed === 1) { // 아래로 이동 중
+    arc(this.x + scl / 2, this.y + scl / 2, scl, scl, radians(135), radians(45));
+  } else if (this.xspeed === -1) { // 왼쪽으로 이동 중
+    arc(this.x + scl / 2, this.y + scl / 2, scl, scl, radians(225), radians(135));
+  } else if (this.xspeed === 1) { // 오른쪽으로 이동 중
+    arc(this.x + scl / 2, this.y + scl / 2, scl, scl, radians(45), radians(-45));
+  } else { // 방향이 없는 경우
+    arc(this.x + scl / 2, this.y + scl / 2, scl, scl, radians(225), radians(135));
+  }
+}
 
 }
